949ebd3a475f63e09df386ce89c606346a55437a
dimachernii <30795909+dimachernii@users.noreply.github.com>
2021-02-10 15:57:47 +0600

    new config

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import './style.scss'
+import  './assets/icon.png'
+import printMe from './print.js';
+console.log('dimasik');
--- webpack.config.js
+++ webpack.config.js
@@ -1,173 +1,76 @@
-const path = require('path');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const  path = require("path");
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const webpackDevServer = require('webpack-dev-server');
-const OptimazeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin');
-const TerserWebpackPlugin = require('terser-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const isDev = process.env.NODE_ENV === 'development';
-
-const optimization = () => {
-    const config = {
-        splitChunks: {
-            chunks: 'all'
-        }
-    }
-
-    if(!isDev) {
-        config.minimizer = [
-            new OptimazeCssAssetsWebpackPlugin(),
-            new TerserWebpackPlugin()
-        ]
-    }
-
-    return config;
-}
-const filename = ext => isDev ? `[name].${ext}` : `[name].[hash].${ext}`
-const cssLoaders = extra => {
-    const loaders =  [{
-        loader: MiniCssExtractPlugin.loader,
-    },'css-loader']
-
-    if(extra) {
-        loaders.push(extra);
-    }
-
-    return loaders;
-}
-const babelOptions = preset => {
-    const opts =
-        {
-            presets: ['@babel/preset-env'],
-            plugins: ['@babel/plugin-proposal-class-properties']
-        }
-
-    if(preset)
-        opts.presets.push(preset);
-
-    return opts
-}
-const plugins = () => {
-    const base = [
-        new HTMLWebpackPlugin({
-            title: 'Webpack Dima',
-            template: './index.html',
-            minify: {
-                collapseWhitespace: !isDev
-            }
-        }),
-        new CleanWebpackPlugin(),
-        new CopyWebpackPlugin({
-            patterns: [
-                {
-                    from: path.resolve(__dirname, 'src/favicon.ico'),
-                    to: path.resolve(__dirname, 'dist')
-                }
-            ]
-        }),
-        new MiniCssExtractPlugin({
-            filename: filename('css')
-        }),
-
-    ]
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 
-
-    return base
-}
 module.exports = {
-    context: path.resolve(__dirname, 'src'),
-    mode: "development",
-    target: 'web',
-    entry: {
-        main: ['@babel/polyfill', './index.jsx'],
-        analytics: './analytics.ts'
-    },
+    entry: './src/index.js',
     output: {
-        filename: filename("js"),
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: '',
-
+        filename: isDev ? `assets/js/[name].js` : `assets/js/[name].[contenthash].js`,
+        path: path.resolve(__dirname, "dist"),
+        publicPath: ''
     },
-    resolve: {
-        alias: {
-            '@models': path.resolve(__dirname, 'src/models'),
-            '@': path.resolve(__dirname, 'src'),
-            process: "process/browser"
-        },
+    optimization: {
+        minimize: true,
+        minimizer: [ new CssMinimizerPlugin({
+            sourceMap: true,
+        })],
+        splitChunks: {
+            chunks: "all"
+        }
     },
-    optimization: optimization(),
+    devtool: isDev ? 'inline-source-map' : false,
     devServer: {
         contentBase: path.join(__dirname, 'dist'),
-        publicPath: '/',
-        open: true,
-        watchContentBase: true,
-        port: 8080,
+        compress: true,
     },
-    devtool: isDev ? 'source-map': false,
-    plugins: plugins(),
+    plugins: [
+       new CleanWebpackPlugin(),
+       new HtmlWebpackPlugin({
+         title: 'Output Management',
+         template: './src/index.html',
+         minify: {
+             collapseWhitespace: !isDev
+          }
+       }),
+       new MiniCssExtractPlugin(
+        {
+            filename: isDev ? `assets/css/[name].css` : `assets/css/[name].[contenthash].css`
+        })
+    ],
+
     module: {
         rules: [
             {
-                test: /\.css$/,
-                use: cssLoaders()
-            },
-            {
-                test: /\.less$/,
-                use: cssLoaders('less-loader')
-            },
-            {
-                test: /\.s[ac]ss$/,
-                use: cssLoaders('sass-loader')
-            },
-            {
-                test: /\.(png|svg|jpg|gif)$/,
-                use: ['file-loader']
-            },
-            {
-                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                test: /\.s[ac]ss$/i,
                 use: [
-                  {
-                    loader: 'file-loader'
-                  }
-                ]
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            publicPath: '../../',
+                        },
+                    },
+                  { loader: 'css-loader', options: { sourceMap: true } },
+                  { loader: 'sass-loader', options: { sourceMap: true } },
+                ],
             },
             {
-                test: /\.xml$/,
-                use: [
-                  {
-                    loader: 'xml-loader'
-                  }
-                ]
-            },
-            {
-                test: /\.m?js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: babelOptions()
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: isDev ?  'assets/images/[name][ext][query]' : 'assets/images/[name].[contenthash][ext][query]'
                 }
             },
             {
-                test: /\.ts$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: babelOptions( "@babel/preset-typescript")
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: isDev ? 'assets/fonts/[name][ext][query]' : 'assets/fonts/[name].[contenthash][ext][query]'
                 }
             },
-            {
-                test: /\.jsx$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: babelOptions( "@babel/preset-react")
-                }
-            }
-
         ]
     }
-
 }
-
